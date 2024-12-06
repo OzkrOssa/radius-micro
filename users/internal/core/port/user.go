744281477daa9b16bdd1ea3d7d66106c0f4983a0
Oscar Ossa <oskrlex17@gmail.com>
2024-12-06 07:18:50 -0500

    feat(service): add user service interface

--- users/internal/core/port/user.go
+++ users/internal/core/port/user.go
@@ -14,3 +14,11 @@ type UserRepository interface {
 	UpdateUser(ctx context.Context, user *domain.User) (*domain.User, error)
 	DeleteUser(ctx context.Context, id uint64) error
 }
+
+type UserService interface {
+	CreateUser(ctx context.Context, user *domain.User) (*domain.User, error)
+	GetUser(ctx context.Context, id uint64) (*domain.User, error)
+	ListUsers(ctx context.Context, skip, limit uint64) ([]domain.User, error)
+	UpdateUser(ctx context.Context, user *domain.User) (*domain.User, error)
+	DeleteUser(ctx context.Context, id uint64) error
+}
